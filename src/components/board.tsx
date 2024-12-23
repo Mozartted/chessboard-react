73092dae7f31157f59e23c6f999122300f0127ba
Chibuike Osita <mozart.osita@gmail.com>
2024-12-23 02:19:27 +0100

    feat(updates): Board structure completed

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -2,12 +2,27 @@ import { FC, ReactNode } from "react";
 
 type BoardPropType = {
   alt: boolean;
+  show_notation_file: boolean;
+  show_notation_rank: boolean;
+  file: number;
+  rank: string;
 };
 
-const Board: FC<BoardPropType> = ({ alt }): ReactNode => {
+const Board: FC<BoardPropType> = ({
+  alt,
+  file,
+  rank,
+  show_notation_file,
+  show_notation_rank,
+}): ReactNode => {
   const boardColor = alt == true ? "square-green" : "square-yellow";
 
-  return <div className={`board ${boardColor}`}></div>;
+  return (
+    <div className={`board ${boardColor}`}>
+      {show_notation_rank ? <span className="file_num">{file}</span> : null}
+      {show_notation_file ? <span className="rank_num">{rank}</span> : null}
+    </div>
+  );
 };
 
 export default Board;
--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -4,25 +4,44 @@ import Board from "./board";
 const Chessboard: FC = () => {
   const squareSizes = 8;
 
+  const notationFile = [1, 2, 3, 4, 5, 6, 7, 8].reverse();
+  const notationRanks = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
   // Board structure logic
   const rows: Array<ReactNode[]> = [];
   let colorAlt = true;
   let startColor = true;
+  let fileCount = 0;
+  let rankCount = 0;
+
   for (let i = 0; i < squareSizes; i++) {
     colorAlt = startColor == true ? true : false;
     rows.push([]);
     for (let j = 0; j < squareSizes; j++) {
-      rows[i].push(<Board alt={colorAlt} />);
+      rows[i].push(
+        <Board
+          show_notation_file={fileCount == squareSizes - 1}
+          show_notation_rank={rankCount == 0}
+          alt={colorAlt}
+          file={notationFile[fileCount]}
+          rank={notationRanks[rankCount]}
+        />,
+      );
       colorAlt = !colorAlt;
+      rankCount++;
     }
+    fileCount++;
+    rankCount = 0;
     startColor = !startColor;
   }
 
   return (
     <>
-      {rows.map((item) => (
-        <div className="rows">{item}</div>
-      ))}
+      <div className="chessboard">
+        {rows.map((item) => (
+          <div className="rows">{item}</div>
+        ))}
+      </div>
     </>
   );
 };
